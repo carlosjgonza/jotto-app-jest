475d52bc356a56c2b03691a7c82095742f491b5b
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-18 17:15:39 -0500

    Including dummy guessed words to test component GuessedWords with latest changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import GuessedWords from './GuessedWords';
 import './App.css';
 
 function App() {
+  const guessedWordsTest = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 }
+  ];
   return (
     <div className="App">
+      <GuessedWords guessedWords={guessedWordsTest} />
     </div>
   );
 }

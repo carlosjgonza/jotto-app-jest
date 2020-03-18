546944b37257dab190d01599f64afe3daf866442
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-18 12:19:54 -0500

    Creating mockups of tests for Congrats component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -3,8 +3,14 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import Congrats from './Congrats';
+import { setup, findByAttr } from '../test/testUtils';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('Render without errors', () => {
+test('Renders without errors', () => {
+  const wrapper = setup(Congrats);
+});
+test('Renders no text when `success` prop is false', () => {
+});
+test('Renders non-empty congrats message when `success` prop is true', () => {
 });

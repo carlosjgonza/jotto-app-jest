461cf6c7bb331b4c976ec9516d4492c0364a38ca
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-18 19:34:48 -0500

    Including action "correctGuests" and test to review this action creator

---
+++ src/actions/index.test.js
@@ -0,0 +1,8 @@
+import { correctGuess, actionTypes } from './index';
+
+describe('CorrectGuess', () => {
+  test('Returns an action with type `CORRECT_GUESS`', () => {
+    const action = correctGuess();
+    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+  });
+});

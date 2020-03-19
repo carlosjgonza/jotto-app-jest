2d681c1e4603c76ed1a44cbe276235b54b71fcdc
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-18 19:19:03 -0500

    Including initial configuration to use redux in app

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+
+/**
+ * @function SuccessReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - Action to be reduced
+ * @returns {boolean} - New Success State
+ */
+export default (state, action) => {
+  return null;
+}

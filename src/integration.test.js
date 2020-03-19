53610c81260aa589c7353166d29228ecb6601fb2
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-19 15:53:10 -0500

    Including guessedWords reducer.
    Including initial integration tests.

---
+++ src/integration.test.js
@@ -0,0 +1,13 @@
+import { storeFactory } from '../test/testUtils';
+import { guessedWord } from './actions';
+
+describe('guessedWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('Updates state correctly for unsuccessful guess', () => {});
+    test('Updates state correctly for successful guess', () => {});
+  });
+  describe('guessed words', () => {
+    test('Updates state correctly for unsuccessful guess', () => {});
+    test('Updates state correctly for successful guess', () => {});
+  });
+});
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function GuessedWordsReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - Action to be reduced
+ * @returns {boolean} - New Success State
+ */
+export default (state=false, action) => {
+  return null
+}

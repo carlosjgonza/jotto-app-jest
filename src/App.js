d883676684ccd5c219389547133bd74169473f75
Carlos Gonzalez <carlos@sancrisoft.com>
2020-03-19 21:01:22 -0500

    Including tests for App component (to validate if redux props are connected)
    Including code to pass test applied

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
+import { getSecretWord } from './actions';
 import './App.css';
 
-function App() {
-  const guessedWordsTest = [
-    { guessedWord: "train", letterMatchCount: 3 },
-    { guessedWord: "agile", letterMatchCount: 1 }
-  ];
-  return (
-    <div className="container">
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords guessedWords={guessedWordsTest} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    const guessedWordsTest = [
+      { guessedWord: "train", letterMatchCount: 3 },
+      { guessedWord: "agile", letterMatchCount: 1 }
+    ];
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={guessedWordsTest} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { success, guessedWords, secretWord } = state;
+  return {
+    guessedWords,
+    secretWord,
+    success,
+  }
+}
+
+export default connect(mapStateToProps, { getSecretWord })(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+
+import { storeFactory } from '../test/testUtils';
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+/**
+ * Setup is a factory function to create a shallowWrapper for App
+ * @function
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState={}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />).dive().dive();
+  return wrapper;
+}
 
-test('Render without errors', () => {
+describe('Redux Props', () => {
+  test('has success redux prop', () => {
+    const success = true;
+    const wrapper = setup ({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test('has `guessedWords` redux prop', () => {
+    const guessedWords = [
+      { guessedWord: "train", letterMatchCount: 3 },
+    ];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toEqual(guessedWords);
+  });
+  test('has `secretWord` redux prop', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test('`getSecretWord` action is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
